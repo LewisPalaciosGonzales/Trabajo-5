24239c7324dada3ea6e3d6ea50116918bc35c895
LewisPalaciosGonzales <57698875+lewispalaciosgonzales@users.noreply.github.com>
2019-12-01 23:08:45 -0500

    numero 1

---
+++ PALaciOS_Gonzales_lewis.py
@@ -0,0 +1,8 @@
+producto="Libro de Auditoria Tributaria"
+type_producto=type(producto)
+producto_es_float=isinstance(producto,float)
+producto_es_str=isinstance(producto,str)
+print("La variable producto es:",type_producto)
+print("La variable es float :",producto_es_float)
+print("La variable es str :",producto_es_str)
+#finif
---
+++ borrados.py
@@ -0,0 +1,8 @@
+producto="Leche"
+type_producto=type(producto)
+producto_es_str=isinstance(producto,str)
+producto_es_int=isinstance(producto,int)
+print("Variables producto es :",type_producto)
+print("Variables es str :",producto_es_str)
+print("Variables es int :",producto_es_int)
+#finif
