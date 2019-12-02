ab17c743ecab35adedbb30938fa8625f209b3e73
LewisPalaciosGonzales <57698875+lewispalaciosgonzales@users.noreply.github.com>
2019-12-01 23:40:26 -0500

    numero 4

--- PALaciOS_Gonzales_lewis.py
+++ PALaciOS_Gonzales_lewis.py
@@ -1,3 +1,4 @@
+#Ejercicio 1
 producto="Libro de Auditoria Tributaria"
 type_producto=type(producto)
 producto_es_float=isinstance(producto,float)
@@ -5,4 +6,34 @@ producto_es_str=isinstance(producto,str)
 print("La variable producto es:",type_producto)
 print("La variable es float :",producto_es_float)
 print("La variable es str :",producto_es_str)
+#finif
+
+#Ejercicio 2
+precio=250
+type_precio=type(precio)
+precio_es_int=isinstance(precio,int)
+precio_es_float=isinstance(precio,float)
+print("La variable precio es :",type_precio)
+print("La variable es int :",precio_es_int)
+print("La variable es float :",precio_es_float)
+#finif
+
+#Ejercio 3
+alumno="Lewis"
+type_alumno=type(alumno)
+alumno_es_float=isinstance(alumno,float)
+alumno_es_str=isinstance(alumno,str)
+print("La variable alumno es :",type_alumno)
+print("La variable es float :",alumno_es_float)
+print("La variable es str :",alumno_es_str)
+#finif
+
+#Ejercicio 4
+visitas=36
+type_visitas=type(visitas)
+visitas_es_bool=isinstance(visitas,bool)
+visitas_es_int=isinstance(visitas,int)
+print("La variable visitas es :",type_visitas)
+print("La variable es bool :",visitas_es_bool)
+print("La variable es int :",visitas_es_int)
 #finif
